77490d790e22dc11f736a10b6524a9c0bd2c76d1
Will MacMillan <macmillan.will@gmail.com>
2015-09-10 13:19:46 -0400

    Update Jetpack & Soliloquy
    Update these two plugins

--- wp-content/plugins/soliloquy/soliloquy.php
+++ wp-content/plugins/soliloquy/soliloquy.php
@@ -5,7 +5,7 @@
  * Description: Soliloquy is best responsive WordPress slider plugin.
  * Author:      Thomas Griffin
  * Author URI:  http://thomasgriffinmedia.com
- * Version:     2.4.3
+ * Version:     2.4.3.1
  * Text Domain: soliloquy
  * Domain Path: languages
  *
@@ -54,7 +54,7 @@ class Soliloquy {
      *
      * @var string
      */
-    public $version = '2.4.3';
+    public $version = '2.4.3.1';
 
     /**
      * The name of the plugin.
